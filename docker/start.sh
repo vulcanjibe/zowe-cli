155e56a72dcc6b906d980a473b97f5b59786f7c1
Vulcanjibe <hcombey@gmail.com>
2020-10-22 21:47:15 +0200

    Create start.sh

---
+++ docker/start.sh
@@ -0,0 +1,3 @@
+echo "TOTO="${HOST_IP}
+zowe  profiles create zosmf-profile mtm2020 --host ${HOST_IP} --port ${HOST_PORT}  --user ${USER} --password ${PASSWORD} --reject-unauthorized false
+zowe profiles create tso-profile mtm2020 -a fb3 
